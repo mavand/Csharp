1af2502d991f046f15dda68bbe94f07c65fa0f21
Valon Breznica <valonbreznica@hotmail.com>
2013-02-07 19:25:50 +0100

    Dataset
    Tog bort dataset. La in en connection i Dal. Nu är det exeptions osv som
    vi måste ha in.

--- Another/Another/dal.cs
+++ Another/Another/dal.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Another
+
 {
     class dal
     {
 
-    }
+        SqlConnection conn = new SqlConnection("Data Source=VALONS;Initial Catalog=gym;Integrated Security=True");
+
+
+   
+}
 }
